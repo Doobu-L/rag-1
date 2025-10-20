5467bf0cdfdf159974cd41157d8d74413ddfa6bc
doobu-l <point455@naver.com>
2025-10-20 12:39:52 +0900

    feat: ChatClient 기본 Advisor 설정 추가 - `MessageChatMemoryAdvisor`와 `MessageWindowChatMemory` 도입 - `InMemoryChatMemoryRepository` 사용하여 기본 채팅 메모리 구성

--- src/main/java/com/prac/rag1/config/ChatClientConfig.java
+++ src/main/java/com/prac/rag1/config/ChatClientConfig.java
@@ -1,6 +1,10 @@
 package com.prac.rag1.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +12,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChatClientConfig {
 
+
     @Bean
-    public ChatClient geminiChatClient(VertexAiGeminiChatModel chatModel) {
-        return ChatClient.builder(chatModel).build();
+    public ChatClient chatClient(VertexAiGeminiChatModel chatModel) {
+        Advisor chatMemoryAdvisor = MessageChatMemoryAdvisor.builder(
+                MessageWindowChatMemory.builder()
+                        .chatMemoryRepository(new InMemoryChatMemoryRepository())
+                        .build())
+                .build();
+        return ChatClient.builder(chatModel).defaultAdvisors(chatMemoryAdvisor).build();
     }
 
 }
